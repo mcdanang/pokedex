adbf65ddf4a5e8b72bc190b038372bdcca24dcec
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2025-04-20 14:38:09 +0700

    add loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Pokeball } from "@/components/pokeball-icon";
+
+export default function Loading() {
+	return (
+		<div
+			className={
+				"flex flex-col items-center justify-center h-screen text-center text-gray-500 space-y-6"
+			}
+		>
+			<div className="animate-spin">
+				<Pokeball className="w-20 h-20 fill-red-400" />
+			</div>
+			<p className="text-lg font-medium">Loading...</p>
+		</div>
+	);
+}
